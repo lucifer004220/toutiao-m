208aee4595ba2ef40bb621544142baf08f0f7df3
wzl <2540804923@qq.com>
2022-09-07 14:55:11 +0800

    项目初始化-完成所有初识化工作

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-guanbi"></i>
+    </div>
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router'
 import store from './store'
 // 加载全局样式
 import './styles/index.less'
+// 加载Vant组件库
+import Vant from 'vant'
+// 加载Vant全局样式
+import 'vant/lib/index.css'
+import 'amfe-flexible'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
